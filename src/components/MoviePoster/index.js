fcb9af94f83f7447476c6f89e63fd0ebc8af781b
Kathiravan <kathirkathir85500@gmail.com>
2025-06-26 20:07:24 +0530

    Update index.js

--- src/components/MoviePoster/index.js
+++ src/components/MoviePoster/index.js
@@ -5,17 +5,20 @@ const MoviePoster = props => {
   const {eachMovie} = props
   const baseUrlForPoster = 'https://image.tmdb.org/t/p/w500'
   const posterImageUrl = `${baseUrlForPoster}${eachMovie.poster_path}`
+
   return (
     <li className="poster-item">
-      {eachMovie.poster_path && (
-        <img
-          src={posterImageUrl}
-          alt={`${eachMovie.title} poster`}
-          className="poster-image"
-        />
-      )}
+      <img
+        src={posterImageUrl}
+        alt={`${eachMovie.title} poster`}
+        className="poster-image"
+        onError={e => {
+          e.target.onerror = null
+          e.target.src = 'https://via.placeholder.com/500x750?text=No+Poster'
+        }}
+      />
       <h1 className="movie-name">{eachMovie.title}</h1>
-      <p className="rating-text">{eachMovie.rating}</p>
+      <p className="rating-text">Rating: {eachMovie.rating}</p>
       <Link to={`/details/${eachMovie.id}`} className="link-style">
         <button className="view-details-btn" type="button">
           View Details
@@ -26,3 +29,4 @@ const MoviePoster = props => {
 }
 
 export default MoviePoster
+
